e6cd251bac37d426a204b82d2677f262bae14717
Alefer Reinert <aleferreinert@gmail.com>
2023-03-09 16:33:57 -0300

    Correção de bugs
    Corrigido estilo de imagem de SectionAbout Project e corrigido tipagem de logo em SectionHero.

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -4,14 +4,14 @@ import Button from 'components/Button'
 import * as S from './styles'
 import { gaEvent } from 'utils/ga'
 import Container from 'components/Container'
-import { HeaderProps, LogoProps } from 'types/api'
+import { HeaderProps, Image } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 
 const onClick = () =>
   gaEvent({ action: 'click', category: 'cta', label: 'hero button' })
 
 type Props = {
-  logo: LogoProps
+  logo: Image
   header: HeaderProps
 }
 
